7b547ceaa5f0b263b12512f207e691acdf41ff22
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2015-04-28 23:46:39 +0300

    Редакторские правки кода: поменяны местами объявление функции и макрос, который ее использует.

--- common/error.h
+++ common/error.h
@@ -10,23 +10,6 @@
 #include <sstream>
 #include <openssl/err.h>
 
-#define ERROR_INTERRUPT( stream ) \
-     do{ \
-          std::ostringstream ostr; \
-          ostr << stream; \
-          error_interrupt( __FILE__, __LINE__, ostr.str().c_str() ); \
-     } \
-     while( false )
-
-
-#define REPORT_MESSAGE( stream ) \
-     do{ \
-          std::ostringstream ostr; \
-          ostr << stream; \
-          report_message( ostr.str().c_str() ); \
-     } \
-     while( false )
-
 inline void error_interrupt( const char* filename, int line, const char* msg )
 {
      fprintf( stderr, "SSL ERROR: [%s:%d]: %s\n", filename, line, msg );
@@ -35,8 +18,10 @@ inline void error_interrupt( const char* filename, int line, const char* msg )
 }
 
 
-inline void report_message( const char* msg )
-{
-     fprintf( stdout, "SSL: %s\n", msg );
-     fflush( stdout );
-}
+#define ERROR_INTERRUPT( stream ) \
+     do{ \
+          std::ostringstream ostr; \
+          ostr << stream; \
+          error_interrupt( __FILE__, __LINE__, ostr.str().c_str() ); \
+     } \
+     while( false )
