8dd15a9d2f54f56948a191396e52dc1a64306f12
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2015-05-02 00:09:32 +0300

    Добавлено логгирование работы клиента, а также функций библиотеки libcommon.so.

--- common/src/tools.cpp
+++ common/src/tools.cpp
@@ -12,6 +12,7 @@
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
 #include <openssl/ssl.h>
+#include <common/logger.h>
 
 namespace openssl {
 
@@ -26,6 +27,10 @@ static constexpr int SUCCESS = 1;
 
 int verification_callback( int ok, X509_STORE_CTX* store )
 {
+     using namespace report;
+
+     Logger( Severity::Debug ) << "verification callback called with success code " << ok;
+
      if( !ok )
      {
           X509* cert = X509_STORE_CTX_get_current_cert( store );
@@ -48,6 +53,10 @@ int verification_callback( int ok, X509_STORE_CTX* store )
 
 static long free_and_return( X509* cert, const long retCode = X509_V_ERR_APPLICATION_VERIFICATION )
 {
+     using namespace report;
+
+     Logger( Severity::Debug ) << "finishing check with ret code " << retCode;
+
      if( cert )
           X509_free( cert );
 
@@ -57,31 +66,53 @@ static long free_and_return( X509* cert, const long retCode = X509_V_ERR_APPLICA
 
 static int checkParameter( const STACK_OF( CONF_VALUE )* val, const std::string& host )
 {
+     using namespace report;
+
+     Logger( Severity::Debug ) << "checking another extension";
+
      for( int j = 0; j < sk_CONF_VALUE_num( val ); ++j )
      {
           CONF_VALUE* nval = sk_CONF_VALUE_value( val, j );
 
+          Logger( Severity::Debug ) << "processing value with name \"" << nval->name << "\" and value \"" << nval->value << "\"";
+
           if( std::string( nval->name ) == "DNS" && std::string( nval->value ) == host )
           {
+               Logger( Severity::Debug ) << "successfully found";
                return consts::SUCCESS;
           }
      }
 
+     Logger( Severity::Debug ) << "finished with error at this time";
      return consts::ERROR;
 }
 
 
 static STACK_OF( CONF_VALUE )* getParameter( X509_EXTENSION* ext )
 {
+     using namespace report;
+
+     Logger( Severity::Debug ) << "getting parameter looking for \"subjectAltName\"";
+
      const std::string extstr = OBJ_nid2sn( OBJ_obj2nid( X509_EXTENSION_get_object( ext ) ) );
 
+     Logger( Severity::Debug ) << "now we have parameter named \"" << extstr << "\"";
+
      if( extstr != "subjectAltName" )
+     {
+          Logger( Severity::Debug ) << "returning with error";
           return nullptr;
+     }
+
+     Logger( Severity::Debug ) << "getting x509v3 extension method";
 
      const X509V3_EXT_METHOD *meth = X509V3_EXT_get( ext );
 
      if( !meth )
+     {
+          Logger( Severity::Debug ) << "returning with error";
           return nullptr;
+     }
 
      const unsigned char* data = ext->value->data;
 
@@ -92,16 +123,24 @@ static STACK_OF( CONF_VALUE )* getParameter( X509_EXTENSION* ext )
                nullptr
           );
 
+     Logger( Severity::Debug ) << "returning with parameter";
+
      return val;
 }
 
 
 static int verifyCertificateExtensions( X509* cert, const std::string& host )
 {
+     using namespace report;
+
+     Logger( Severity::Debug ) << "verifying certificate with extensions";
+
      const int extcount = X509_get_ext_count( cert );
 
      if( extcount > 0 )
      {
+          Logger( Severity::Debug ) << "found extensions: " << extcount;
+
           for( int i = 0; i < extcount; ++i )
           {
                STACK_OF( CONF_VALUE )* val = getParameter( X509_get_ext( cert, i ) );
@@ -114,16 +153,27 @@ static int verifyCertificateExtensions( X509* cert, const std::string& host )
           }
      }
 
+     Logger( Severity::Debug ) << "exit with error; sorry...";
+
      return consts::ERROR;
 }
 
 
 long post_connection_check( SSL* ssl, const char* host )
 {
+     using namespace report;
+
+     Logger( Severity::Info ) << "perfoming post connection check";
+
+     Logger( Severity::Debug ) << "getting peer certificate";
+
      X509* cert = SSL_get_peer_certificate( ssl );
 
      if( !cert || !host )
+     {
+          Logger( Severity::Debug ) << "no peer cert found or host is set to nullptr";
           return free_and_return( cert );
+     }
 
      const int ok = verifyCertificateExtensions( cert, host );
 
@@ -133,11 +183,20 @@ long post_connection_check( SSL* ssl, const char* host )
 
      if( !ok && subj && X509_NAME_get_text_by_NID( subj, NID_commonName, data, sizeof( data ) ) )
      {
+          Logger( Severity::Debug ) << "trying get hostname from common name certificate section";
+
           data[ sizeof( data ) - 1 ] = 0;
 
+          Logger( Severity::Debug ) << "common name section has value \"" << data << "\"";
+
           if( strcasecmp( data, host ) != 0 )
+          {
+               Logger( Severity::Debug ) << "returning with nothing, sorry...";
                return free_and_return( cert );
+          }
      }
+
+     Logger( Severity::Debug ) << "there must bee success";
 
      return free_and_return( cert, SSL_get_verify_result( ssl ) );
 }
--- ssl_client/src/client.cpp
+++ ssl_client/src/client.cpp
@@ -8,34 +8,52 @@
 #include "client.h"
 
 #include <cstring>
+#include <boost/thread.hpp>
 #include <common/error.h>
 #include <common/tools.h>
+#include <common/logger.h>
 
 namespace openssl {
 
 SSL_CTX* get_client_ctx( const boost::filesystem::path& cert, const boost::filesystem::path& caFile )
 {
+     using namespace report;
+
      static constexpr int SUCCESS = 1;
+
+     Logger( Severity::Info ) << "initializing SSL context";
 
      SSL_CTX* ctx = SSL_CTX_new( SSLv23_method() );
 
      if( !ctx )
           ERROR_INTERRUPT( "cannot initialize SSL context" );
 
+     Logger( Severity::Debug ) << "loading SSL verify locations with file " << caFile;
+
      if( SSL_CTX_load_verify_locations( ctx, caFile.c_str(), nullptr ) != SUCCESS )
           ERROR_INTERRUPT( "cannot load CA file " << caFile );
+
+     Logger( Severity::Debug ) << "loading SSL default verify paths";
 
      if( SSL_CTX_set_default_verify_paths( ctx ) != SUCCESS )
           ERROR_INTERRUPT( "cannot load default CA paths" );
 
+     Logger( Severity::Debug ) << "setting certificate chain file " << cert;
+
      if( SSL_CTX_use_certificate_chain_file( ctx, cert.c_str() ) != SUCCESS )
           ERROR_INTERRUPT( "cannot load certificate from file " << cert );
+
+     Logger( Severity::Debug ) << "loading private key from file" << cert;
 
      if( SSL_CTX_use_PrivateKey_file( ctx, cert.c_str(), SSL_FILETYPE_PEM ) != SUCCESS )
           ERROR_INTERRUPT( "cannot load private key from file " << cert );
 
+     Logger( Severity::Debug ) << "setting verification parameters, verification callback and verification depth";
+
      SSL_CTX_set_verify( ctx, SSL_VERIFY_PEER, verification_callback );
      SSL_CTX_set_verify_depth( ctx, 4 );
+
+     Logger( Severity::Info ) << "SSL context successfully initialized";
 
      return ctx;
 }
@@ -43,6 +61,10 @@ SSL_CTX* get_client_ctx( const boost::filesystem::path& cert, const boost::files
 
 int do_client_loop( SSL* ssl )
 {
+     using namespace report;
+
+     Logger( Severity::Info ) << "[thread: " << boost::this_thread::get_id() << "] " << "running client main loop";
+
      const int buff_size = 80;
      char buff[ buff_size ] = { 0 };
 
@@ -53,12 +75,25 @@ int do_client_loop( SSL* ssl )
 
           for( int total_written = 0, written = 0; total_written < buff_size; total_written += written )
           {
+               Logger( Severity::Debug )
+                    << "[thread: " << boost::this_thread::get_id() << "] "
+                    << "write to SSL connection, total written " << total_written << " bytes";
+
                written = SSL_write( ssl, buff + total_written, strlen( buff ) - total_written );
 
+               Logger( Severity::Debug )
+                    << "[thread: " << boost::this_thread::get_id() << "] "
+                    << "written " << written << " bytes";
+
                if( written <= 0 )
+               {
+                    Logger( Severity::Info ) << "[thread: " << boost::this_thread::get_id() << "] " << "client loop finished";
                     return 0;
+               }
           }
      }
+
+     Logger( Severity::Info ) << "[thread: " << boost::this_thread::get_id() << "] " << "client loop finished";
 
      return 1;
 }
